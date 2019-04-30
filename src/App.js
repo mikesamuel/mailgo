6fcb5114d02bd8467a1334da6477c3fd435cf374
Matteo Manzinello <matteo@manzinello.com>
2019-04-30 21:06:24 +0200

    aggiunta immagine in react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src="mailgo.png" />
         <p>mailgo</p>
         <a
           className="App-link"
