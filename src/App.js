016fe77f556719bf0eb05de183b9bc6c8e724f0c
Matteo Manzinello <matteo@manzinello.com>
2019-04-30 15:03:23 +0200

    bozza logo emoji

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src="mailgo.png" />
         <p>mailgo (WIP)</p>
         <a
           className="App-link"
