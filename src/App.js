2854e5e2ce1b459d47e245b3fa70f7b1de4c45b4
Matteo Manzinello <matteo@manzinello.com>
2019-05-06 09:13:57 +0200

    stateful App

--- src/App.js
+++ src/App.js
@@ -2,45 +2,47 @@ import React from "react";
 
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="mailgo.png" />
-        <h1>mailgo</h1>
-        <code className="code">
-          WIP,{" "}
-          <a class="App-link" href="https://github.com/manzinello/mailgo">
-            GitHub
-          </a>
-          <br />
-          <br />
-          a classic mailto
-          <br />
-          <a class="App-link no-mailgo" href="mailto:matteo@manzinello.dev">
-            matteo@manzinello.dev
-          </a>
-          <br />
-          with <strong>mailgo</strong>
-          <br />
-          <a class="App-link" href="mailto:matteo@manzinello.dev">
-            matteo@manzinello.dev
-          </a>
-          <br />
-          <br />
-          <u>
-            less then <strong>7KB</strong>
-          </u>
-          <br />
-          <br />a project by
-          <br />
-          <a class="App-link" href="https://matteomanzinello.com">
-            matteo manzinello
-          </a>
-        </code>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src="mailgo.png" />
+          <h1>mailgo</h1>
+          <code className="code">
+            WIP,{" "}
+            <a class="App-link" href="https://github.com/manzinello/mailgo">
+              GitHub
+            </a>
+            <br />
+            <br />
+            a classic mailto
+            <br />
+            <a class="App-link no-mailgo" href="mailto:matteo@manzinello.dev">
+              matteo@manzinello.dev
+            </a>
+            <br />
+            with <strong>mailgo</strong>
+            <br />
+            <a class="App-link" href="mailto:matteo@manzinello.dev">
+              matteo@manzinello.dev
+            </a>
+            <br />
+            <br />
+            <u>
+              less then <strong>7KB</strong>
+            </u>
+            <br />
+            <br />a project by
+            <br />
+            <a class="App-link" href="https://matteomanzinello.com">
+              matteo manzinello
+            </a>
+          </code>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
